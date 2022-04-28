d1f831c12d905a0aecb5222917fb34e8c17e7ad1
Xero1997 <104274072+Xero1997@users.noreply.github.com>
2022-04-28 02:06:51 -0300

    Agregacion de dos metodos de inserción atras hacia adelante y abajo hacia arriba

--- src/SopaDeLetras.java
+++ src/SopaDeLetras.java
@@ -25,23 +25,22 @@ public class SopaDeLetras {
 	};
 	static int tamano = 12;			// Tamaño del cuadro que se usa
 	static char [][] sopa;			// Matriz de la sopa de letras (sin instanciar)
-	
+
 	// Funcion principal
 	public static void main(String[] args) {
-		
+
 		int contPalabrasPuestas = 0;				// Cuenta cuantas palabras fueron ingresadas en la sopa
 		sopa = new char[tamano][tamano];			// Creación de la matriz
 		Random r = new Random(System.currentTimeMillis());
+
 		while (contPalabrasPuestas<palabrasSopa.length)
 		{
 			int [] posicion = {0, 0};						// Vector posicion que posiciona la palabra
-			boolean orientacion = false;			// Orientacion que define la palabra
-			
-			orientacion = r.nextBoolean();
-			
-			posicion[0] = r.nextInt(12);
-			posicion[1] = r.nextInt(12);
-			
+			int orientacion =  r.nextInt(4)+1;			// Orientacion que define la palabra
+
+			posicion[0] = r.nextInt(tamano);
+			posicion[1] = r.nextInt(tamano);
+
 			if (analizarPalabra(palabrasSopa[contPalabrasPuestas], orientacion, posicion))
 			{
 				ponerPalabra(palabrasSopa[contPalabrasPuestas], orientacion, posicion);
@@ -51,18 +50,16 @@ public class SopaDeLetras {
 		SopaDeLetras.rellenarSopa();			
 		SopaDeLetras.dibujarSopa();
 	}
-	
-	
-	
+
+
 	private static void rellenarSopa() {
 		/*
 		 * Funcion que rellena cada caracter de la sopa.
 		 * Ejecutarla solo una vez hecha la colocación de las palabras
 		 * dentro de la sopa de letras
 		 */
-		
 		Random r = new Random(System.currentTimeMillis());		// Creacion de una variable aleatoria.
-		
+
 		// Se usa dos estructuras for para acceder a los
 		// distintos cuadros de la sopa y llenarlos
 		for (int i=0; i<tamano; i++)
@@ -70,23 +67,19 @@ public class SopaDeLetras {
 			//r = new Random(System.currentTimeMillis());	// Regeneracion semilla numero aleatorio
 			for (int j=0; j<tamano; j++)
 			{
-				/*
-				 * TODO: BUG que no permite que la generacion aletoria
-				 * de caracteres sea correcta. Resolver
-				 */
-				
+
 				if (sopa[i][j] == '\u0000')
 					sopa[i][j] = (char)(97+r.nextInt(26));			// Numero aleatorio entre 97 y 122, que luego
-																// transforma a su equivalente numerico en ASCII
+				// transforma a su equivalente numerico en ASCII
 			}
 		}
 	}
-	
+
 	private static void dibujarSopa() {
 		/*
 		 * Funcion que dibuja la sopa en pantalla.
 		 */
-		
+
 		for (int i=0; i<tamano; i++)
 		{
 			for (int j=0; j<tamano; j++)
@@ -96,8 +89,8 @@ public class SopaDeLetras {
 			System.out.println("|");
 		}
 	}
-	
-	private static boolean analizarPalabra(String palabra, boolean orientacion, int [] posicion)
+
+	private static boolean analizarPalabra(String palabra, int orientacion, int [] posicion)
 	{
 		/*
 		 * Funcion que elige una palabra del array de palabras,
@@ -109,19 +102,32 @@ public class SopaDeLetras {
 			try
 			{
 				// Esta cosa ^ es un operador XOR
-				if (orientacion)
-				{
+
+				switch (orientacion) {
+				case 1:
 					if(!(sopa[posicion[0]][posicion[1]+i] != '\u0000' ^ sopa[posicion[0]][posicion[1]+i] != palabra.charAt(i)))
 					{
 						return false;
 					}
-				}
-				else
-				{
+					break;
+				case 2:
 					if(!(sopa[posicion[0]+i][posicion[1]] != '\u0000' ^ sopa[posicion[0]][posicion[1]+i] != palabra.charAt(i)))
 					{
 						return false;
 					}
+					break;
+				case 3:
+					if(!(sopa[posicion[0]-i][posicion[1]] != '\u0000' ^ sopa[posicion[0]][posicion[1]-i] != palabra.charAt(i)))
+					{
+						return false;
+					}
+					break;
+				case 4:
+					if(!(sopa[posicion[0]][posicion[1]-i] != '\u0000' ^ sopa[posicion[0]][posicion[1]-i] != palabra.charAt(i)))
+					{
+						return false;
+					}
+					break;
 				}
 			}
 			catch (ArrayIndexOutOfBoundsException e) {
@@ -131,18 +137,28 @@ public class SopaDeLetras {
 		}
 		return true;
 	}
-	
-	private static void ponerPalabra(String palabra, boolean orientacion, int [] posicion)
+
+	private static void ponerPalabra(String palabra, int orientacion, int [] posicion)
 	{
 		for(int i=0; i<palabra.length(); i++)
 		{
-			if (orientacion)
-			{
-				sopa[posicion[0]][posicion[1]+i] = palabra.charAt(i);
-			}
-			else
-			{
-				sopa[posicion[0]+i][posicion[1]] = palabra.charAt(i);
+
+			switch (orientacion) {
+			case 1:
+				sopa[posicion[0]][posicion[1]+i] = palabra.toUpperCase().charAt(i);
+				break;
+			case 2:
+				sopa[posicion[0]+i][posicion[1]] = palabra.toUpperCase().charAt(i);
+
+				break;
+			case 3:
+				sopa[posicion[0]-i][posicion[1]] = palabra.toUpperCase().charAt(i);
+
+				break;
+			case 4:
+				sopa[posicion[0]][posicion[1]-i] = palabra.toUpperCase().charAt(i);
+				break;
+
 			}
 		}
 	}
